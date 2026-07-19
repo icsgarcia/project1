ff3b47ecb3af2eb1eb28616ee33b1ed261fcaf5e
Ivan Garcia <icsgarcia2002@gmail.com>
2026-07-19 10:20:09 +0800

    add details on External Links Page

--- client/src/pages/user/ExternalLinksPage.tsx
+++ client/src/pages/user/ExternalLinksPage.tsx
@@ -1,13 +1,32 @@
 import DashboardLayout from "@/layouts/DashboardLayout";
 import {
     Card,
-    CardAction,
     CardContent,
     CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+
+const externalLinks = [
+    {
+        title: "University of the Assumption",
+        desc: "Official Website of the University of the Assumption",
+        image: "/logos/ua-logo.png",
+        url: "https://web.ua.edu.ph/",
+    },
+    {
+        title: "University of the Assumption - SERP",
+        desc: "Official Website of the University of the Assumption - SERP",
+        image: "/logos/ua-logo.png",
+        url: "https://serp.ua.edu.ph/serp/Gate/UASFP.Login.aspx",
+    },
+    {
+        title: "RSOinTrack",
+        desc: "Official Website of RSOinTrack",
+        image: "/logos/RSOinTrack-logo.png",
+        url: "#",
+    },
+];
 
 const ExternalLinksPage = () => {
     return (
@@ -15,33 +34,27 @@ const ExternalLinksPage = () => {
             <div className="p-4">
                 <div className="flex flex-col gap-4">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Title</CardTitle>
-                                <CardDescription>Description</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <div>Content</div>
-                            </CardContent>
-                        </Card>
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Title</CardTitle>
-                                <CardDescription>Description</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <div>Content</div>
-                            </CardContent>
-                        </Card>
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Title</CardTitle>
-                                <CardDescription>Description</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <div>Content</div>
-                            </CardContent>
-                        </Card>
+                        {externalLinks.map((externalLink) => (
+                            <Card
+                                onClick={() =>
+                                    window.open(externalLink.url, "_blank")
+                                }
+                                className="relative w-full pt-0 cursor-pointer"
+                            >
+                                <div className="absolute inset-0 z-30 aspect-video" />
+                                <img
+                                    src={externalLink.image}
+                                    alt={externalLink.title}
+                                    className="relative z-20 aspect-video w-full object-cover"
+                                />
+                                <CardHeader>
+                                    <CardTitle>{externalLink.title}</CardTitle>
+                                    <CardDescription>
+                                        {externalLink.desc}
+                                    </CardDescription>
+                                </CardHeader>
+                            </Card>
+                        ))}
                     </div>
                 </div>
             </div>
